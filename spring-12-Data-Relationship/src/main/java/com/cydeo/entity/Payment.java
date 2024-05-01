68736a86bef9b47d7eaabcd8c52acee459e19c2b
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-05-01 19:51:59 -0400

    Spring11DataTableCreation

--- spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
@@ -1,8 +1,10 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.Status;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -10,24 +12,34 @@ import java.time.LocalDate;
 @Entity
 @NoArgsConstructor
 @Data
-@Table(name="payments")
+@Table(name = "payments")
 public class Payment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(columnDefinition = "DATE")
     private LocalDate createdDate;
-    private BigDecimal amount;
-    @Enumerated(EnumType.STRING)
-    private String paymentStatus;
 
-    public Payment(LocalDate createdDate, BigDecimal amount, String paymentStatus) {
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private Status paymentStatus;
+
+    //    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    @JoinColumn(name = "payment_detail_id")
+    private PaymentDetail paymentDetail;
+
+    @ManyToOne
+    private Merchant merchant;
+
+    public Payment(LocalDate createdDate, BigDecimal amount, Status paymentStatus) {
         this.createdDate = createdDate;
         this.amount = amount;
         this.paymentStatus = paymentStatus;
     }
 }
-
 
 
