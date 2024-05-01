714b81d65ab80771ba288660f78a950bd34dda73
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-04-30 20:50:10 -0400

    Spring11DataTableCreation

---
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO EMPLOYEES(id)
+VALUES(1,'Mike');
+
+INSERT INTO students(email)
+VALUES('Mike','Smith','mike@cydeo.com');
+
+INSERT INTO students(email)
+VALUES('Tom','Hanks','tom@cydeo.com');
+
+
