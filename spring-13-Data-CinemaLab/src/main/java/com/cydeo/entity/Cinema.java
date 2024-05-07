05f415e6b173c352656788e8d8a3b0af11a7f5af
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-05-07 19:27:55 -0400

    spring 13

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Cinema.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Cinema.java
@@ -1,4 +1,19 @@
 package com.cydeo.entity;
 
-public class Cinema {
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Cinema extends BaseEntity {
+
+    private String name;
+    private String sponsoredName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Location location;
 }
--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Ticket.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Ticket.java
@@ -1,4 +1,24 @@
 package com.cydeo.entity;
 
-public class Ticket {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Ticket extends BaseEntity{
+
+    private Integer seatNumber;
+    private Integer rowNumber;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime dateTime;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MovieCinema movieCinema;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User userAccount;
+
+
 }
+
