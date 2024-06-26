d9e43bd6e728185a672274bea1bf19899b6ff9ac
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-06-26 19:47:07 -0400

    spring 06

--- spring-06-Core-BeanScope/src/main/java/com/cydeo/repository/DBCommentRepository.java
+++ spring-06-Core-BeanScope/src/main/java/com/cydeo/repository/DBCommentRepository.java
@@ -4,10 +4,10 @@ import com.cydeo.model.Comment;
 import org.springframework.stereotype.Component;
 
 @Component
-public interface DBCommentRepository implements CommentRepository{
+public class DBCommentRepository implements CommentRepository{
 
     @Override
-    public default void storeComment(Comment comment) {
+    public void storeComment(Comment comment) {
         System.out.println("Storing comment:" + comment.getText());
     }
 }
