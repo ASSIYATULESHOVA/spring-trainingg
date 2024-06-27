6260fcae3bdfcd6eccab3216e758d5c630bcbd33
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-06-27 17:42:38 -0400

    spring

--- spring-03-Wiring/src/main/java/com/cydeo/monitorfactory/Monitor.java
+++ spring-03-Wiring/src/main/java/com/cydeo/monitorfactory/Monitor.java
@@ -10,7 +10,6 @@ public abstract class Monitor {
     private String model;
     private String manufacturer;
     private int size;
-
     public Monitor(String model, String manufacturer, int size) {
         this.model = model;
         this.manufacturer = manufacturer;
